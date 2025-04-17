bc11b3643acbc5dda352a83b097d7aa11192e32a
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2025-04-17 12:12:37 +0100

    Enhance item management functionality with error messaging and delete option

--- script.js
+++ script.js
@@ -1,33 +1,62 @@
 const cart = []
 
+
 const addItem = () => {
     if(item.value === '') {
-        alert('Haba now, fill in the inputs joor')
+        // alert('Haba now, fill in the inputs joor')
+        errorMsg.style.display = 'block'
     } else {
+        errorMsg.style.display = 'none'
         cart.push(item.value)
         document.getElementById('item').value = ''
         console.log(cart);
-
+        if(cart.length >= 1) {
+            btnDelete.style.display = 'block'
+        }
         displayItem()
+    }
+}
+
+const deleteAnyItem = () => {
+    if(cart.length >= 1) {
+        const userValue = Number(prompt("Enter the number you want to delete"))
+        // console.log(userValue);
+        if (userValue < 1) {
+            console.log('Numbering starts from 1');
+        } else if(userValue <= cart.length) {
+            cart.splice(userValue-1, 1)
+            displayItem()
+            if(cart.length < 1) {
+            btnDelete.style.display = 'none'
+            }
+        }
+        else {
+            alert('Invalid number entered')
+        }
+        // cart.splice(index,1)
     }
 }
 
 const displayItem = () => {
     show.innerHTML = ""
-        for(let i=0;i < cart.length;i++) {
-            show.innerHTML += `
-                <p>${i+1}. ${cart[i]}</p>
-            `
-        }
+        // for(let i=0;i < cart.length;i++) {
+        //     show.innerHTML += `
+        //         <p>${i+1}. ${cart[i]}</p>
+        //     `
+        // }
+    cart.map((data, index)=>{
+        console.log(data);
+        show.innerHTML += `
+            <p>${index+1}. ${data}</p>
+        `
+    })
 }
 
-// const test1 = ['Segun', 'Tolu', 'Kola', 'Jerry']
-// console.log(test1.length);
+// let allStudents = ['Mary', 'Sultan', 'Ayanfe', 'Taiwo']
+// allStudents.splice(1,3)
+// console.log(allStudents);
 
-// for(i=0; i < test1.length; i++) {
-//     console.log(test1[i]);
-    
-// }
+
 
 
 
