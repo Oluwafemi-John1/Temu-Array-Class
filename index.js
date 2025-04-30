3cdfcb3c0f545908704b02b68172f5ea0e269a76
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2025-04-30 11:21:07 +0100

    Enhance sign-up functionality with input validation and success messaging

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
+const toast = (text, background, color, position='right') => {
+    Toastify({
+        text,
+        duration: 3000,
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position, // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+            background,
+            color,
+        },
+        onClick: function () { } // Callback after click
+    }).showToast();
+}
+
 const allUsers = []
 
 const signUp = () => {
-    window.location.href = 'signin.html'
+    if (firstName.value === '' || lastName.value === '' || email.value === '' || password.value === '') {
+        // errorMsg.style.display = 'block'
+        toast('Haba now, fill in the inputs joor😠👿', '#f00', '#fff')
+    } else {
+        errorMsg.style.display = 'none'
+
+        const fName = document.getElementById('firstName').value
+        const lName = document.getElementById('lastName').value
+        const mail = document.getElementById('email').value
+        const pass = document.getElementById('password').value
+
+        const userObj = { fName, lName, mail, pass }
+        // console.log(userObj);
+        allUsers.push(userObj)
+        toast('Sign up successful😁', '#006400', '#fff')
+        console.log(allUsers);
+
+        document.getElementById('firstName').value = ''
+        document.getElementById('lastName').value = ''
+        document.getElementById('email').value = ''
+        document.getElementById('password').value = ''
+    }
 }
