97b3ae159ce0ed30d3ff8ea4024c1f39c824bebb
Oyeniran Oluwafemi John <oyeniranoluwafemi36@gmail.com>
2025-05-06 13:34:29 +0100

    Refactor code for consistency and readability in checkPerson, addItem, and deleteAnyItem functions

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const checkPerson = () => {
-    if(localStorage.person) {
+    if (localStorage.person) {
         const signedInUser = JSON.parse(localStorage.getItem('person'))
         console.log(signedInUser);
         showPerson.innerHTML = `<h4 class="my-3 text-center">Welcome ${signedInUser.fName} ${signedInUser.lName},</h4>`
-        signin} else {
+        signin
+    } else {
         body.innerHTML = `<h4 class="my-3 text-center">You are not signed in, redirecting you to sign in...</h4>`
-        setTimeout(()=>{
+        setTimeout(() => {
             window.location.href = 'signin.html'
         }, 1500)
     }
@@ -21,7 +22,7 @@ const signOut = () => {
 const cart = []
 
 const addItem = () => {
-    if(item.value === '') {
+    if (item.value === '') {
         // alert('Haba now, fill in the inputs joor')
         errorMsg.style.display = 'block'
     } else {
@@ -29,7 +30,7 @@ const addItem = () => {
         cart.push(item.value)
         document.getElementById('item').value = ''
         console.log(cart);
-        if(cart.length >= 1) {
+        if (cart.length >= 1) {
             btnDelete.style.display = 'block'
         }
         displayItem()
@@ -37,16 +38,16 @@ const addItem = () => {
 }
 
 const deleteAnyItem = () => {
-    if(cart.length >= 1) {
+    if (cart.length >= 1) {
         const userValue = Number(prompt("Enter the number you want to delete"))
         // console.log(userValue);
         if (userValue < 1) {
             console.log('Numbering starts from 1');
-        } else if(userValue <= cart.length) {
-            cart.splice(userValue-1, 1)
+        } else if (userValue <= cart.length) {
+            cart.splice(userValue - 1, 1)
             displayItem()
-            if(cart.length < 1) {
-            btnDelete.style.display = 'none'
+            if (cart.length < 1) {
+                btnDelete.style.display = 'none'
             }
         }
         else {
@@ -58,15 +59,15 @@ const deleteAnyItem = () => {
 
 const displayItem = () => {
     show.innerHTML = ""
-        // for(let i=0;i < cart.length;i++) {
-        //     show.innerHTML += `
-        //         <p>${i+1}. ${cart[i]}</p>
-        //     `
-        // }
-    cart.map((data, index)=>{
+    // for(let i=0;i < cart.length;i++) {
+    //     show.innerHTML += `
+    //         <p>${i+1}. ${cart[i]}</p>
+    //     `
+    // }
+    cart.map((data, index) => {
         console.log(data);
         show.innerHTML += `
-            <p>${index+1}. ${data}</p>
+            <p>${index + 1}. ${data}</p>
         `
     })
 }
